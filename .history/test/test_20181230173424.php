3d57bec0dfd71c8f7fb740253eafd00f64386400
mwpo79 <mwpo79@users.noreply.github.com>
2022-06-21 09:56:18 +0200

    Delete .history directory

--- .history/test/test_20181230173424.php
+++
@@ -1,8 +0,0 @@
-<?
-echo __DIR__ ;
-die ;
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
-
-use HelloWorld\SayHello;
-
-echo SayHello::world();
--- .history/test/test_20181230174203.php
+++
@@ -1,6 +0,0 @@
-<?
-require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
-
-use HelloWorld\SayHello;
-
-echo SayHello::toPhp();
